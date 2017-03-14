0df8dc60b26481b34d340cbf46e8cc3fd5d7a96b
Cindy Yu <cindy.yu@teradata.com>
2017-03-13 19:04:40 -0700

    Initial Commit

---
+++ tipCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipCalculator
+//
+//  Created by Yu, Cindy on 3/13/17.
+//  Copyright © 2017 Yu, Cindy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
